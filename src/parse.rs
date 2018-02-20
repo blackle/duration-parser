5d95b7e07270c15380527578ef8ed9849dba7998
blackle <isabelle@blackle-mori.com>
2018-02-20 17:56:56 +0100

    Rustfmt

--- src/parse.rs
+++ src/parse.rs
@@ -1,13 +1,13 @@
 use regex::Regex;
 use error::Error;
 
-const SEC_IN_MIN : u64 = 60;
-const SEC_IN_HR : u64 = SEC_IN_MIN * 60;
-const SEC_IN_DAY : u64 = SEC_IN_HR * 24;
-const SEC_IN_WK : u64 = SEC_IN_DAY * 7;
-const SEC_IN_YR : u64 = SEC_IN_DAY * 365;
+const SEC_IN_MIN: u64 = 60;
+const SEC_IN_HR: u64 = SEC_IN_MIN * 60;
+const SEC_IN_DAY: u64 = SEC_IN_HR * 24;
+const SEC_IN_WK: u64 = SEC_IN_DAY * 7;
+const SEC_IN_YR: u64 = SEC_IN_DAY * 365;
 
-fn multiplier_from_unit(units : &str) -> Result<u64, Error> {
+fn multiplier_from_unit(units: &str) -> Result<u64, Error> {
 	match units {
 		"s" => Ok(1),
 		"m" => Ok(SEC_IN_MIN),
@@ -15,11 +15,11 @@ fn multiplier_from_unit(units : &str) -> Result<u64, Error> {
 		"d" => Ok(SEC_IN_DAY),
 		"w" => Ok(SEC_IN_WK),
 		"y" => Ok(SEC_IN_YR),
-		_ => Err(Error::UnknownSuffix)
+		_ => Err(Error::UnknownSuffix),
 	}
 }
 
-pub fn parse_duration(string : &str) -> Result<u64, Error> {
+pub fn parse_duration(string: &str) -> Result<u64, Error> {
 	lazy_static! {
 		static ref TIME_RE: Regex = Regex::new(r"(?P<value>\d+)(?P<units>[a-z])").unwrap();
 	}
@@ -27,26 +27,26 @@ pub fn parse_duration(string : &str) -> Result<u64, Error> {
 	let caps_iter = TIME_RE.captures_iter(string);
 
 	let mut expected_length = 0;
-	let mut total_duration_in_ms : u64 = 0;
+	let mut total_duration_in_ms: u64 = 0;
 	for caps in caps_iter {
 		let value_str = &caps["value"];
 		let units = &caps["units"];
 
 		expected_length += value_str.len() + units.len();
 
-		let value : u64 = match value_str.parse() {
+		let value: u64 = match value_str.parse() {
 			Ok(val) => val,
-			Err(_) => return Err(Error::UnparsableNumber)
+			Err(_) => return Err(Error::UnparsableNumber),
 		};
 
 		let duration_in_ms = match value.checked_mul(multiplier_from_unit(units)?) {
 			Some(val) => val,
-			None => return Err(Error::Overflow)
+			None => return Err(Error::Overflow),
 		};
 
 		total_duration_in_ms = match total_duration_in_ms.checked_add(duration_in_ms) {
 			Some(val) => val,
-			None => return Err(Error::Overflow)
+			None => return Err(Error::Overflow),
 		};
 	}
 
--- src/test.rs
+++ src/test.rs
@@ -1,53 +1,57 @@
 #[cfg(test)]
 mod tests {
-		use parse::parse_duration;
-		use error::Error;
-		#[test]
+	use parse::parse_duration;
+	use error::Error;
+	#[test]
 
-		fn five_minutes() {
-				let result = parse_duration(&String::from("5m")).unwrap();
-				assert!(result == 5 * 60);
-		}
+	fn five_minutes() {
+		let result = parse_duration(&String::from("5m")).unwrap();
+		assert!(result == 5 * 60);
+	}
 
-		#[test]
-		fn combination() {
-				let result = parse_duration(&String::from("1y2w5d3h42m10s")).unwrap();
-				assert!(result == 1 * 60*60*24*365 + 2 * 60*60*24*7 + 5 * 60*60*24 + 3 * 60*60 + 42 * 60 + 10);
-		}
+	#[test]
+	fn combination() {
+		let result = parse_duration(&String::from("1y2w5d3h42m10s")).unwrap();
+		assert!(
+			result
+				== 1 * 60 * 60 * 24 * 365 + 2 * 60 * 60 * 24 * 7 + 5 * 60 * 60 * 24 + 3 * 60 * 60
+					+ 42 * 60 + 10
+		);
+	}
 
-		#[test]
-		fn overflow() {
-				let err = parse_duration(&String::from("1000000000000y")).unwrap_err();
-				assert!(err == Error::Overflow);
-		}
+	#[test]
+	fn overflow() {
+		let err = parse_duration(&String::from("1000000000000y")).unwrap_err();
+		assert!(err == Error::Overflow);
+	}
 
-		#[test]
-		fn incorrect_suffix() {
-				let err = parse_duration(&String::from("9f")).unwrap_err();
-				assert!(err == Error::UnknownSuffix);
-		}
+	#[test]
+	fn incorrect_suffix() {
+		let err = parse_duration(&String::from("9f")).unwrap_err();
+		assert!(err == Error::UnknownSuffix);
+	}
 
-		#[test]
-		fn unparsable_components() {
-				let err = parse_duration(&String::from("1d5ss")).unwrap_err();
-				assert!(err == Error::UnparsableExtras);
-		}
+	#[test]
+	fn unparsable_components() {
+		let err = parse_duration(&String::from("1d5ss")).unwrap_err();
+		assert!(err == Error::UnparsableExtras);
+	}
 
-		#[test]
-		fn unparsable_number() {
-				let err = parse_duration(&String::from("999999999999999999999999999999999y")).unwrap_err();
-				assert!(err == Error::UnparsableNumber);
-		}
+	#[test]
+	fn unparsable_number() {
+		let err = parse_duration(&String::from("999999999999999999999999999999999y")).unwrap_err();
+		assert!(err == Error::UnparsableNumber);
+	}
 
-		#[test]
-		fn empty_string() {
-				let err = parse_duration(&String::from("")).unwrap_err();
-				assert!(err == Error::NoData);
-		}
+	#[test]
+	fn empty_string() {
+		let err = parse_duration(&String::from("")).unwrap_err();
+		assert!(err == Error::NoData);
+	}
 
-		#[test]
-		fn nonsense_string() {
-				let err = parse_duration(&String::from("I'm gfucking jormsting?")).unwrap_err();
-				assert!(err == Error::NoData);
-		}
+	#[test]
+	fn nonsense_string() {
+		let err = parse_duration(&String::from("I'm gfucking jormsting?")).unwrap_err();
+		assert!(err == Error::NoData);
+	}
 }
